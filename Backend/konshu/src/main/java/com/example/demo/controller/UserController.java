f789d0d3d631596d0915300259293294c67a8b31
Miarevalo3 <80972437+Miarevalo3@users.noreply.github.com>
2022-06-05 08:46:16 -0500

    Update UserController.java

--- Backend/konshu/src/main/java/com/example/demo/controller/UserController.java
+++ Backend/konshu/src/main/java/com/example/demo/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Nutrient;
+
 import com.example.demo.model.User;
 
 import com.example.demo.repository.UserRepository;
